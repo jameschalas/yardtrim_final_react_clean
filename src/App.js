fd9bd9839ec0244be74051950a3f2d42834f39e0
jameschalas <jamesxalas@gmail.com>
2025-04-23 16:02:37 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
 import Gallery from './components/Gallery';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Services from './pages/Services'; // or wherever your Services.jsx is
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/services" element={<Services />} />
+        <Route path="/lawn-care" element={<Services />} />
+        <Route path="/tree-removal" element={<Services />} />
+        <Route path="/yard-cleanup" element={<Services />} />
+        {/* other routes */}
+      </Routes>
+    </Router>
+  );
+}
 
 function App() {
   return (
